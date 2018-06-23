9445c46db81b6345515687de3ea4eae45605f661
Sabina Shrestha <sabina.pari@yahoo.com>
2018-06-23 15:33:03 -0600

    add brewary

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -15,6 +15,9 @@ class NavBar extends Component {
           <Link to='/'>
             <Menu.Item name='home' active={this.activeItem('/')} />
           </Link>
+          <Link to='/'>
+            <Menu.Item name='brewary' active={this.activeItem('/brewary')} />
+          </Link>
           <Menu.Menu position='right'>
             <Link to='/'>
               <Menu.Item name='DPS React Assessment' />
